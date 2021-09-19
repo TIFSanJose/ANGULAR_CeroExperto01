a514e509d2442ac2d81567565ef9b2bb596efc8c
ramon <ramon@ramon>
2021-09-19 17:19:26 -0300

    v17 funciones: parametros obligatorios, opcional y por defecto

--- typescript/app.js
+++ typescript/app.js
@@ -1,3 +1,18 @@
 "use strict";
 (function () {
+    /**
+     * Funcones: parametros Obligatorios, Opcionales y PorDefecto.
+     * Se debe segir ese orden de definicion de parametros en las funciones
+     *
+     */
+    function activar(obligatorio, opcional, porDefecto) {
+        if (porDefecto === void 0) { porDefecto = 'Batisenal'; }
+        if (opcional) {
+            console.log(" \n                " + obligatorio + " activo en la " + opcional + " la " + porDefecto);
+        }
+        else {
+            console.log(" \n                " + obligatorio + " activo la " + porDefecto + " ");
+        }
+    }
+    activar('Gordon', '', 'Batisenal');
 }());
