6eafd56bc339dbd89a548b329806b3e87162a23c
ramon <ramon@ramon>
2021-09-22 11:21:34 -0300

    v22 Interfaces concepto

--- typescript/app.js
+++ typescript/app.js
@@ -1,3 +1,31 @@
 "use strict";
 (() => {
+    /**
+     * Definiendo un objeto Xmen de tipo XMEN.
+     */
+    const xmen = {
+        nombre: 'Wolverine',
+        edad: 60,
+        poder: 'garras',
+        lanzarPoder() {
+            return `mostrar ${this.poder}`;
+        }
+    };
+    const xmen1 = {
+        nombre: 'ciclope',
+        edad: 21,
+        poder: 'lazer',
+        lanzarPoder() {
+            return `${this.poder} por los hojos`;
+        }
+    };
+    const enviarMision = (nombre) => {
+        console.log(`Enviar ${nombre} a la mision`);
+    };
+    const volverAlCuartel = (nombre) => {
+        console.log(`${nombre} debe volver al cuartel `);
+    };
+    enviarMision(xmen.nombre);
+    enviarMision(xmen1.lanzarPoder());
+    volverAlCuartel(xmen.nombre);
 })();
