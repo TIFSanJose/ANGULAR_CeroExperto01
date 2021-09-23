2f267153d9cbf62bdb3b686262459dd75807342f
ramon <ramon@ramon>
2021-09-23 09:13:29 -0300

    v31 Ejecicio practico. resolucion esta bien. incluye videos 29 y 30

--- typescript/app.js
+++ typescript/app.js
@@ -1,25 +1,46 @@
 "use strict";
 (() => {
-    /**
-     *
-     * Tipado de retorno de la funcion, es decir definir que tipo dato debe retornar una funcion.
-     * TypeScript en algunas funciones puede inferir el tipo de dato de retorno como en los dos
-     * primeros ejemplos a continuacion
-     */
-    const suma = (a, b) => a + b;
-    const nombre = () => 'hola soy Jorge';
-    /**
-     *
-     * Para los casos que no puede inferir el tipo de retorno.
-     *
-     */
-    const saldo = () => {
-        return new Promise((resolve, reject) => {
-            resolve('Promesa resuelta');
-            reject('No se pude devolver lo prometido');
-        });
+    // Uso de Let y Const
+    // var nombre = 'Ricardo Tapia';
+    // var edad = 23;
+    const nombre = 'Ricardo Tapia';
+    let edad = 23;
+    // var PERSONAJE = {
+    //   nombre: nombre,
+    //   edad: edad
+    // };
+    const PERSONAJE = {
+        nombre: 'nombre',
+        edad: 23
     };
-    console.log(suma(1, 3));
-    console.log(nombre());
-    console.log(saldo());
+    // Cree una interfaz que sirva para validar el siguiente objeto
+    var batman = {
+        nombre: 'Bruno Díaz',
+        artesMarciales: ['Karate', 'Aikido', 'Wing Chun', 'Jiu-Jitsu']
+    };
+    // Convertir esta funcion a una funcion de flecha
+    function resultadoDoble(a, b) {
+        return (a + b) * 2;
+    }
+    // Función con parametros obligatorios, opcionales y por defecto
+    // donde NOMBRE = obligatorio
+    //       PODER  = opcional
+    //       ARMA   = por defecto = 'arco'
+    function getAvenger(nombre, poder, arma) {
+        var mensaje;
+        if (poder) {
+            mensaje = nombre + ' tiene el poder de: ' + poder + ' y un arma: ' + arma;
+        }
+        else {
+            mensaje = nombre + ' tiene un ' + poder;
+        }
+    }
+    ;
+    // Cree una clase que permita manejar la siguiente estructura
+    // La clase se debe de llamar rectangulo,
+    // debe de tener dos propiedades:
+    //   * base
+    //   * altura
+    // También un método que calcule el área  =  base * altura,
+    // ese método debe de retornar un numero.
 })();
